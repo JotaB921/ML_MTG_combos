2a95efa04b863c0484bb85034a5c9bd05061cdb7
Jaylton Araujo <araujojaylton@gmail.com>
2022-02-19 10:18:23 -0400

    Merge datasets spellbook e MTG_JSON

--- Projeto_R_MTG_combos/processamento_dados.R
+++ Projeto_R_MTG_combos/processamento_dados.R
@@ -1,17 +1,32 @@
 
 library(tidyverse)
 library(readr)
-Magic_complete_database <- read_delim("~/Downloads/Magic complete database.csv", 
-                                      delim = ";", escape_double = FALSE, trim_ws = TRUE)
+library(pastecs)
 
-library(rjson)
-mtg_json_file <- "/home/jaylton/Área de Trabalho/Temporários/AllPrintings.json"
+MTG_JSON_Dream_Trawler <- 
+  read_csv("D:/Projetos gyro4data/ML_MTG_combos/Dados/MTJ_JSON_Dream Trawler.csv")
+spellbook_DB_combos <- 
+  read_csv("D:/Projetos gyro4data/ML_MTG_combos/Dados/Commander Spellbook Database - combos.csv")
 
-mtg_json_data <- fromJSON(paste(readLines(mtg_json_file), collapse=""))
+stk_spell<- stack(spellbook_DB_combos[2:11])
+stk_spell <- na.omit(stk_spell)
+lista_cartas_combo <- unique(stk_spell)
+lista_cartas_combo <- data.frame(lista_cartas_combo[,-2])
+colnames(lista_cartas_combo) <- c("nomes_cartas")
 
-library(jsonlite)
+teste <- left_join(lista_cartas_combo, MTG_JSON_Dream_Trawler, 
+                   by = c("nomes_cartas" = "completeName"))
 
-data_raw <- enframe(unlist(mtg_json_data))
+sum(is.na(teste$name))
 
-#teste do git hub
-x <- data.frame()
+new_DF <- teste
+
+new_DF[is.na(new_DF)] = 0
+
+lista_cartas_faltando <- subset(new_DF, name == 0)
+lista_cartas_faltando <- data.frame(lista_cartas_faltando$nomes_cartas)
+
+write.csv(lista_cartas_faltando,
+          "D:/Projetos gyro4data/ML_MTG_combos/Dados/lista_cartas_faltando.csv", 
+          row.names = FALSE)
+
