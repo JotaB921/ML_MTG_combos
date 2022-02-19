d786dc006be9d286dd4de9cb7344f8e17379cb01
Jaylton Araujo <araujojaylton@gmail.com>
2022-02-19 18:23:43 -0400

    Filtragem das cartas e combos no R

--- Projeto_R_MTG_combos/processamento_dados.R
+++ Projeto_R_MTG_combos/processamento_dados.R
@@ -7,26 +7,39 @@ MTG_JSON_Dream_Trawler <-
   read_csv("D:/Projetos gyro4data/ML_MTG_combos/Dados/MTJ_JSON_Dream Trawler.csv")
 spellbook_DB_combos <- 
   read_csv("D:/Projetos gyro4data/ML_MTG_combos/Dados/Commander Spellbook Database - combos.csv")
+EDHREC_Number_of_Decks <- read_csv("D:/Projetos gyro4data/ML_MTG_combos/EDHREC Number of Decks.csv")
 
 stk_spell<- stack(spellbook_DB_combos[2:11])
 stk_spell <- na.omit(stk_spell)
-lista_cartas_combo <- unique(stk_spell)
-lista_cartas_combo <- data.frame(lista_cartas_combo[,-2])
-colnames(lista_cartas_combo) <- c("nomes_cartas")
+lista_cartas_combo <- count(stk_spell, "values")
+colnames(lista_cartas_combo) <- c("nomes_cartas", "n_combos")
 
 teste <- left_join(lista_cartas_combo, MTG_JSON_Dream_Trawler, 
-                   by = c("nomes_cartas" = "completeName"))
+                   by = c("nomes_cartas" = "name"))
 
-sum(is.na(teste$name))
+db_edh_spell <- left_join(EDHREC_Number_of_Decks, spellbook_DB_combos, 
+                          by = c("ID_spellbook" = "ID"))
 
-new_DF <- teste
-
-new_DF[is.na(new_DF)] = 0
-
-lista_cartas_faltando <- subset(new_DF, name == 0)
-lista_cartas_faltando <- data.frame(lista_cartas_faltando$nomes_cartas)
-
-write.csv(lista_cartas_faltando,
-          "D:/Projetos gyro4data/ML_MTG_combos/Dados/lista_cartas_faltando.csv", 
+quantos_combos <- count(stk_spell, "values")
+  
+write.csv(lista_cartas_combo,
+          "D:/Projetos gyro4data/ML_MTG_combos/Dados/lista_cartas_combo.csv", 
           row.names = FALSE)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
